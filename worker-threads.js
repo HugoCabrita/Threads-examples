b0b001c85d5b3a48f5c12ee60efe9aa892ed6a4b
hmp_cabrita <hmp.cabrita@gmail.com>
2022-05-28 15:31:32 +0100

    Section 11 Worker threads

---
+++ worker-threads.js
@@ -0,0 +1,21 @@
+const {
+    isMainThread,
+    workerData,
+     Worker
+    } = require ('worker_threads');
+
+if (isMainThread)
+{
+    console.log(`Main thread! process ID: ${process.pid}`);
+    new Worker(__filename,{
+        workerData: [7,6,2,3]
+    });
+    new Worker(__filename,{
+        workerData: [1,3,4,3]
+    });
+} else
+{
+    console.log(`worker! process ID: ${process.pid}`);
+    console.log(`${workerData} sorted is ${workerData.sort()}`);
+    // [7,6,2,3].sort()
+}
